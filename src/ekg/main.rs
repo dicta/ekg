89fa032a32e8573d8ddd9c0b816e322f5de8237c
Dan Bryant <dan@deepwavedigital.com>
2019-12-13 16:24:15 -0500

    Combine jetson thermal measurements into one metric with named sensor values

--- src/ekg/main.rs
+++ src/ekg/main.rs
@@ -5,13 +5,16 @@ use ekg_jetson::*;
 fn main() -> Result<(), Box<dyn Error>> {
     let therm = Thermal::new();
 
-    for e in therm.sensors {
-        println!("temp,sensor={} temp={:.2}", &e.sensor_name, &e.temp_degrees)
-    }
+    let therm_meas_strs : Vec<String> =
+        therm.sensors.iter().map(|e| {
+            format!("{}={:.2}", &e.sensor_name, &e.temp_degrees)
+        }).collect();
+
+    println!("temp,sensor=jetson_thermal {}", therm_meas_strs.join(","));
 
     let gpu = Gpu::new()?;
 
-    println!("gpu,gpu=jetson freq={:.2} load={:.3}", gpu.gpu_freq, gpu.gpu_load);
+    println!("gpu,gpu=jetson freq={:.2},load={:.3}", gpu.gpu_freq, gpu.gpu_load);
 
     Ok(())
 }
