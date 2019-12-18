77d10ec377412504a868267b6317affcc959bb6e
Dan Bryant <dan@deepwavedigital.com>
2019-12-18 13:48:46 -0500

    Report GPU utilization as a percentage

--- src/ekg_jetson/gpu.rs
+++ src/ekg_jetson/gpu.rs
@@ -11,7 +11,11 @@ use std::io;
 
 use crate::util;
 
+// The clock rate is given in Hz and requires superuser access to read this file
+// (anything in /sys/kernel/debug requires elevated permissions)
 const GPU_CLK_FILE: &str = "/sys/kernel/debug/bpmp/debug/clk/gpcclk/rate";
+
+// The load value in sysfs is an integer from 0-1000 as a load percent, value 1000 = 100% load
 const GPU_LOAD_FILE: &str = "/sys/devices/17000000.gp10b/load";
 
 
@@ -24,10 +28,11 @@ pub struct Gpu {
 
 impl Gpu {
     pub fn new() -> io::Result<Self> {
+        let gpu_load: f32 = util::value_from_file(GPU_LOAD_FILE)?;
         Ok(Gpu {
             timestamp: Utc::now(),
             gpu_freq:  util::value_from_file(GPU_CLK_FILE)?,
-            gpu_load:  util::value_from_file(GPU_LOAD_FILE)?
+            gpu_load:  gpu_load / 10.0
         })
     }
 }
