eb0da7b9d8f4ec2f7afd0da93f98a4e8f11b9f0d
Dan Bryant <dan@deepwavedigital.com>
2024-08-14 15:52:43 -0400

    Add preliminary support for Xavier GPU load measurement

--- src/ekg_jetson/gpu.rs
+++ src/ekg_jetson/gpu.rs
@@ -16,8 +16,8 @@ use crate::util;
 const GPU_CLK_FILE: &str = "/sys/kernel/debug/bpmp/debug/clk/gpcclk/rate";
 
 // The load value in sysfs is an integer from 0-1000 as a load percent, value 1000 = 100% load
-const GPU_LOAD_FILE: &str = "/sys/devices/17000000.gp10b/load";
-
+const GPU_LOAD_FILE_TX2: &str = "/sys/devices/17000000.gp10b/load";
+const GPU_LOAD_FILE_XAVIER: &str = "/sys/devices/17000000.gv11b/load";
 
 #[derive(Debug, Clone)]
 pub struct Gpu {
@@ -28,11 +28,20 @@ pub struct Gpu {
 
 impl Gpu {
     pub fn new() -> io::Result<Self> {
-        let gpu_load: f32 = util::value_from_file(GPU_LOAD_FILE)?;
-        Ok(Gpu {
-            timestamp: Utc::now(),
-            gpu_freq:  util::value_from_file(GPU_CLK_FILE)?,
-            gpu_load:  gpu_load / 10.0
-        })
+        if let Ok(gpu_load) = util::value_from_file::<f32, _>(GPU_LOAD_FILE_XAVIER) {
+            Ok(Gpu {
+                timestamp: Utc::now(),
+                gpu_freq:  util::value_from_file(GPU_CLK_FILE)?,
+                gpu_load:  gpu_load / 10.0
+            })
+        } else if let Ok(gpu_load) = util::value_from_file::<f32, _>(GPU_LOAD_FILE_TX2) {
+            Ok(Gpu {
+                timestamp: Utc::now(),
+                gpu_freq:  util::value_from_file(GPU_CLK_FILE)?,
+                gpu_load:  gpu_load / 10.0
+            })
+        } else {
+            Err(io::Error::new(io::ErrorKind::Other, "Could not read GPU load"))
+        }
     }
 }
